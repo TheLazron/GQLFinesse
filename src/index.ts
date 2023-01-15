e6e2bbc7316c38c903f6ec84d77be1c230a85b93
TheLazron <kaushik.rohan31@gmail.com>
2023-01-15 23:30:06 +0530

    configuration changes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-console.log("Hello World")
+import {ApolloServer} from '@apollo/server'
+import {startStandaloneServer} from '@apollo/server/standalone'
+import {gql} from 'graphql-tag'
+import {Client} from '@apperate/iexjs'
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+
+const typeDefs = gql`
+  type Query{
+    hello: String!   
+  }
+`
+
+const resolvers = {
+    Query: {
+      hello: ()=>"world"
+    },
+  };
+
+
+const client = new Client({api_token: "TOKEN", version:"VERSION"});
+client.quote({symbol:"AAPL"}).then(res=>console.log(res));
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+const 
+
+const {url} = await startStandaloneServer(server,{listen: {port:4000}})
+
+console.log("Server started at port 4000")
