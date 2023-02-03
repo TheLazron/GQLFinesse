83645ac5ca44987b35047ce19a6ef60c8f3fb338
TheLazron <kaushik.rohan31@gmail.com>
2023-02-03 11:43:30 +0530

    schema defined and quote mutation declared

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import {ApolloServer} from '@apollo/server'
 import {startStandaloneServer} from '@apollo/server/standalone'
 import {gql} from 'graphql-tag'
-import {Client} from '@apperate/iexjs'
+import {Client} from '@apperate/iexjs';
+import GraphQLJSON from 'graphql-type-json';
+import fetch from 'node-fetch';
+
+import * as dotenv from 'dotenv';
+
+
+dotenv.config();
 const books = [
     {
       title: 'The Awakening',
@@ -15,29 +22,63 @@ const books = [
 
 
 const typeDefs = gql`
+  scalar JSON
+
   type Query{
     hello: String!   
+  }
+
+  type Mutation{
+    quote(symbol: String!): JSON
   }
 `
 
 const resolvers = {
+  JSON: GraphQLJSON,
+
     Query: {
       hello: ()=>"world"
     },
+    Mutation: {
+      quote: (_: any, {symbol}:{symbol: string} )=>{
+        return getQuote(symbol);
+      }
+    }
   };
 
 
-const client = new Client({api_token: "TOKEN", version:"VERSION"});
-client.quote({symbol:"AAPL"}).then(res=>console.log(res));
+const client = new Client({api_token: "sk_e5e69772acc34c609a62e78faaf6741f", version:"v1"});
 
+
+const getQuote= (symbol: String)=>{
+
+  // return fetch("https://jsonplaceholder.typicode.com/todos/1")
+  // .then(data=>{
+  //     return data.json().then(json=>json);
+  // });
+
+ return client.quote({symbol:symbol})
+ 
+//  .then((res:undefined)=>{
+//     console.log("result",res)
+//   }).catch((err:Error)=>{
+//     console.log("error", err);
+//   })
+  
+}
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 });
 
-const 
+const startServer= async ()=>{
 
-const {url} = await startStandaloneServer(server,{listen: {port:4000}})
+  const {url} = await startStandaloneServer(server,{listen: {port:8800}})
 
+
+}
+
+
+startServer();
 console.log("Server started at port 4000")
